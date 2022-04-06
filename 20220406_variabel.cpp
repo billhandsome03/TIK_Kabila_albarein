175a037240ba7ab84ebbe48138f7395d9c46587e
kabilaalbarein_ <103086021+billhandsome03@users.noreply.github.com>
2022-04-06 11:08:49 +0800

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream> 
+using namespace std;
+int main() {//begin of function
+    float angka ;
+    string nama; //deklarasi variabel
+    nama = "kabila albarein"; //masukan value ke dalam variabel
+    angka = 10.21;
+    cout<<nama<<nama<<nama<<nama<<nama<<endl; //menampilkan variabel
+    cout<<angka + 3.14;
+    
+}//end of function
