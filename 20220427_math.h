a56b858a7cb44b389f36896e7d0019bbb730a1b1
kabilaalbarein_ <103086021+billhandsome03@users.noreply.github.com>
2022-04-27 10:15:22 +0800

    Create 20220427_math.h

---
+++ 20220427_math.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std; 
+int main(){
+    int number1 = 0;
+    while (number1++ <10){ 
+        cout<<"1 "<<" * "<<number1<<" = "<<number1*1<<endl;
+    }
+    cout<<endl<<endl;
+    int number2 = 0
+    while (number2++ <10){
+        cout<<"2 "<<" * "<<number2<<" = "<<number2*2<<endl;
+    }
+    
+    return 0;
+ }   
+    
